e3b9ffd3f52f07619339bfe554494017fca2254d
ababutski <alexey.babutski@gmail.com>
2021-11-30 16:14:46 +0300

    rollback all changes

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,8 +8,6 @@ namespace TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //smoll change
-            // one more change
             var connectionString = "Compress=True;CheckCompressedHash=False;Compressor=lz4;Host=clickhouse;Port=9000;User=default;Password=;SocketTimeout=600000;Database=Test;SslEnabled=True";
             var expectedSettings = new ClickHouseConnectionSettings(connectionString);
         }
