d6b0da504334da821153ef33baeddc44c10c7616
ababutski <alexey.babutski@gmail.com>
2021-11-30 16:03:04 +0300

    one more change for submodules

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -9,6 +9,7 @@ namespace TestApp
         {
             Console.WriteLine("Hello World!");
             //smoll change
+            // one more change
             var connectionString = "Compress=True;CheckCompressedHash=False;Compressor=lz4;Host=clickhouse;Port=9000;User=default;Password=;SocketTimeout=600000;Database=Test;SslEnabled=True";
             var expectedSettings = new ClickHouseConnectionSettings(connectionString);
         }
