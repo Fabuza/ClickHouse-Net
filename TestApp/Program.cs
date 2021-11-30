fa1ac31ea6a82a437c34b7d93bee88d053ebd3f4
ababutski <alexey.babutski@gmail.com>
2021-11-30 16:00:27 +0300

    here we have small change for submodule testing

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,6 +8,7 @@ namespace TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //smoll change
             var connectionString = "Compress=True;CheckCompressedHash=False;Compressor=lz4;Host=clickhouse;Port=9000;User=default;Password=;SocketTimeout=600000;Database=Test;SslEnabled=True";
             var expectedSettings = new ClickHouseConnectionSettings(connectionString);
         }
